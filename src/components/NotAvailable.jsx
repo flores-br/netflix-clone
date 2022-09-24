72c8fd136ff30f5fe38018524f0b8db88651264e
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-24 15:43:48 -0400

    NotAvailable component

---
+++ src/components/NotAvailable.jsx
@@ -0,0 +1,10 @@
+const NotAvailable = () => {
+  return (
+    <h1 className='not-available'>
+      No Movies avaialble for the selected genre. Please select a different
+      genre.
+    </h1>
+  );
+};
+
+export default NotAvailable;
