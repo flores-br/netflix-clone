f1d66bc20978a03090b3dd723250daf5aa6cb8ae
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-24 14:55:10 -0400

    video player page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Netflix from './pages/Netflix';
+import Player from './pages/Player';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/signup' element={<Signup />} />
+        <Route exact path='/player' element={<Player />} />
         <Route exact path='/' element={<Netflix />} />
       </Routes>
     </BrowserRouter>
