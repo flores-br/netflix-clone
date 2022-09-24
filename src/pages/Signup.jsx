2cc7251dcc28b049ced1597f4622149cb10a569d
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-24 14:08:40 -0400

    sign up page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config';
+
 import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   position: relative;
@@ -12,14 +21,81 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-colums: ${({ showPassword }) =>
+          showPassword ? '1fr 1fr' : '2fr 1fr'};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
   }
 `;
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, currentUser => {
+    if (currentUser) navigate('/');
+  });
+
   return (
-    <Container>
+    <Container showPassword={showPassword}>
       <BackgroundImage />
       <div className='content'>
+        <Header login />
         <div className='body flex column a-center j-center'>
           <div className='text flex column'>
             <h1>Unlimited Movies, TV shows and more</h1>
@@ -29,11 +105,37 @@ const Signup = () => {
             </h6>
           </div>
           <div className='form'>
-            <input type='email' name='email' placeholder='Email Address' />
-            <input type='password' name='password' placeholder='Password' />
-            <button>Get Started</button>
+            <input
+              type='email'
+              name='email'
+              placeholder='Email Address'
+              value={formValues.email}
+              onChange={e =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            {showPassword && (
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={formValues.password}
+                onChange={e =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
           </div>
-          <button>Log In</button>
+          <button onClick={handleSignIn}>Sign Up</button>
         </div>
       </div>
     </Container>
